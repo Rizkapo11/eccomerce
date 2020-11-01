389338fe749cb73f1dbbdc60ce1a69807e654e67
rizka <rizkaputriocta11@gmail.com>
2020-11-01 18:49:37 +0700

    Merge pull request #2 from Mohammadrezafah/bugfix/style-html
    fixbug and style

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,5 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join (BASE_DIR), ('static')
+    os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
